872530ab3da2340c42e801dee029bdfb9ca0cae7
Ya Jin <qooq147852@gmail.com>
2022-01-01 18:02:05 +0800

    LoG finish~

--- edge_detection.py
+++ edge_detection.py
@@ -1,8 +1,14 @@
+import math
 import numpy as np
-import scipy as sp
-import scipy.ndimage as nd
 from PIL import Image
 import matplotlib.pyplot as plt
+
+Lap_1 = np.ones((3,3))*(-1)
+Lap_1[1,1] = 8
+
+Lap_2 = np.ones((3,3))*(-1)
+Lap_2[1,1] = -4
+Lap_2[0,1] = Lap_2[1,0] = Lap_2[1,2] = Lap_2[2,1] = 22
 
 def sobel_edge_detection(img_array):
    
@@ -22,48 +28,69 @@ def sobel_edge_detection(img_array):
     
     return result
 
-def any_neighbor_zero(img, i, j):
-    for k in range(-1,2):
-      for l in range(-1,2):
-         if img[i+k, j+k] == 0:
-            return True
-    return False
-def zero_crossing(img):
-    img[img > 0] = 1
-    img[img < 0] = 0
-    out_img = np.zeros(img.shape)
-    for i in range(1,img.shape[0]-1):
-        for j in range(1,img.shape[1]-1):
-            if img[i,j] > 0 and any_neighbor_zero(img, i, j):
-                out_img[i,j] = 255
-    return out_img
+def create_gaussian_filter(x, y, sigma):
+    gaussian_filter = np.zeros((x + x, y + x))
+    total = 0
+    for i in range(-x, x):
+        for j in range(-y, y):
+            a = (i ** 2) + (j ** 2)
+            b = 2 * (sigma ** 2)
+            gaussian_filter[i + 3, j + 3] = np.exp(-((a * a) / b)) / math.pi * b
+            total = total + gaussian_filter[i + 3, j + 3]
+
+    gaussian_filter = (gaussian_filter[:] / total)
+
+    return gaussian_filter
+def pad(img, padding):
+    rows, cols = np.shape(img)
+    padded = np.zeros((rows + padding, cols + padding))
+
+    for i in range(rows + padding):
+        for j in range(cols + padding):
+            if i != 0 and i != rows + padding - 1 and j != 0 and j != cols + padding - 1:
+                padded[i, j] = img[i - padding - 1, j - padding - 1]
+
+    return padded
+def apply_gaussian_filter(img, sigma):
+    padded = pad(img, 4)
+    rows, cols = padded.shape
+    blurred = np.zeros((rows - 6, cols - 6))
+
+    gaussian = create_gaussian_filter(3, 3, sigma)
+
+    for i in range(rows):
+        for j in range(cols):
+            pixel = 0
+            if i < rows - 6 and j < cols - 6:
+                for k in range(6):
+                    for l in range(6):
+                        pixel = pixel + (gaussian[k, l] * padded[i + k, j + l])
+                blurred[i, j] = int(round(pixel))
+
+    return blurred
+
 
 def LoG_edge_detection(img_array):
-    result  = zero_crossing(nd.gaussian_laplace(img_array, sigma=3))
+    sigma = 2
+    kernel = Lap_1
+    blurred = apply_gaussian_filter(img_array, sigma)
+    blur_padded = pad(blurred, 2)
+    rows, cols = blur_padded.shape
+    x = len(kernel)
+    y = len(kernel)
+
+    result = np.zeros((rows - 2, cols - 2))
+
+    for i in range(rows):
+        for j in range(cols):
+            pixel = 0
+            if i != 0 and i < rows - 2 and j != 0 and j < cols - 2:
+                for k in range(3):
+                    for l in range(3):
+                        pixel = pixel + (kernel[k][l] * blur_padded[i + k, j + l])
+                result[i, j] = int(round(pixel))
     
-    # LoG = nd.gaussian_laplace(img_array , 2)
-    # thres = np.absolute(LoG).mean() * 0.75
-    # output = sp.zeros(LoG.shape)
-    # w = output.shape[1]
-    # h = output.shape[0]
-
-    # for y in range(1, h - 1):
-    #     for x in range(1, w - 1):
-    #         patch = LoG[y-1:y+2, x-1:x+2]
-    #         p = LoG[y, x]
-    #         maxP = patch.max()
-    #         minP = patch.min()
-    #         if (p > 0):
-    #             zeroCross = True if minP < 0 else False
-    #         else:
-    #             zeroCross = True if maxP > 0 else False
-    #         if ((maxP - minP) > thres) and zeroCross:
-    #             output[y, x] = 1
-
-    # plt.imshow(output)
-    # plt.show()
     return result
-    # return output
 
 def showResult(img):
     img_array = np.array(img)
@@ -84,9 +111,10 @@ def showResult(img):
     plt.axis('off')
     plt.imshow(Image.fromarray(np.uint8(LoG_edge_detection(img_array))))
     
-
-    
-    plt.show()
     return
 
+showResult(Image.open('HW4_test_image\image1.jpg').convert('L'))
 showResult(Image.open('HW4_test_image\image2.jpg').convert('L'))
+showResult(Image.open('HW4_test_image\image3.jpg').convert('L'))
+
+plt.show()
